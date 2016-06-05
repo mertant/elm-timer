4a855d4d3ceeac4ca0b3515b4e644f944c167ca2
mertant <teo.mertanen@aalto.fi>
2016-06-05 22:14:44 +0300

    Change button colors depending on the state

--- Timer.elm
+++ Timer.elm
@@ -112,37 +112,70 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  div [ style [ ("text-align", "center"), ("margin", "5rem"), ("max-width", "40rem")] ]
-  [ h1 [] [ text (toString model.intervalType) ]
+  div [ mainStyle ]
+  [ h1 [ titleStyle ] [ text title ]
   , div [ flexRow ]
-    [ buttonView (Reset Tomato) "Tomato (work)"
-    , buttonView (Reset ShortBreak) "Short break (DEBUG)"
-    , buttonView (Reset LongBreak) "Long break"
+    [ buttonView (Reset Tomato) (model.intervalType == Tomato) "Tomato (work)"--(Reset Tomato) (True) (if (model.intervalType == Tomato) then "TOMATO" else "Tomato (work)" )
+    , buttonView (Reset ShortBreak) (model.intervalType == ShortBreak) "Short break (DEBUG)"
+    , buttonView (Reset LongBreak) (model.intervalType == LongBreak) "Long break"
     ]
   , (timerView model)
   , div [ flexRow ]
-    [ buttonView ToggleStop "Stop/Start"
-    , buttonView (Reset model.intervalType) "Reset"
+    [ buttonView ToggleStop (model.stopped) (if model.stopped then "Start" else "Stop")
+    , buttonView (Reset model.intervalType) False "Reset"
     ]
   ]
 
-buttonView : Msg -> String -> Html Msg
-buttonView event string =
-  button [ buttonStyle, onClick event ] [ text string ]
+title: String
+title = "Task Timer"
+
+buttonView : Msg -> Bool -> String -> Html Msg
+buttonView event isSelected string =
+  button [ buttonStyle isSelected, onClick event ] [ text string ]
 
 timerView : Model -> Html Msg
 timerView model =
-  div [ timerStyle ] [ text  ( formatTime model.timer) ]
+  div [ timerStyle model.stopped ] [ text  ( formatTime model.timer) ]
 
 
 
 -- STYLES --
+light: String
+light = "#1598b2"
+dark: String
+dark = "#001330"
+orangeish: String
+orangeish = "#ff6c11"
 
+mainStyle : Html.Attribute a
+mainStyle =
+  style [ ("text-align", "center")
+        , ("margin", "5rem")
+        , ("max-width", "40rem")
+        , ("font-family", "sans-serif")
+        ]
+
+titleStyle: Html.Attribute a
+titleStyle =
+   style [ ("font-size", "5rem")
+
+         , ("color", orangeish)
+         ]
+
+flexRow : Html.Attribute a
 flexRow =
-  style [ ("flex-direction", "row"), ("display", "flex")]
+  style [ ("flex-direction", "row")
+        , ("display", "flex") ]
 
-buttonStyle =
-  style [ ("flex", "1"), ("font-size", "2rem") ]
+buttonStyle : Bool -> Html.Attribute a
+buttonStyle isSelected =
+  style [ ("flex", "1")
+        , ("font-size", "2rem")
+        , ("color", "white")
+        , ("background-color", (if (isSelected) then light else dark)) ]
 
-timerStyle =
-  style [ ("font-size", "8rem"), ("font-family", "sans-serif") ]
+timerStyle : Bool -> Html.Attribute a
+timerStyle stopped =
+  style [ ("font-size", "8rem")
+        , ("color", if (stopped) then orangeish else light)
+        ]
